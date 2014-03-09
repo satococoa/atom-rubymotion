4a0d4f0f7c714e8d23a7a0d48a6bd639d4f13f02
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-03-09 21:01:06 +0900

    Create snippets_generator

---
+++ misc/snippets_genarator.rb
@@ -0,0 +1,87 @@
+require 'bundler/setup'
+Bundler.require
+require 'pp'
+
+DATADIR = '/Library/RubyMotion/data'
+
+BRIDGE_SUPPORT_FILES = {
+  ios: Dir.glob(DATADIR + '/ios/7.1/BridgeSupport/*.bridgesupport'),
+  osx: Dir.glob(DATADIR + '/osx/10.9/BridgeSupport/*.bridgesupport')
+}
+
+def generate_snippet(name, prefix, body)
+  <<-EOS
+  '#{name}':
+    'prefix': '#{prefix}'
+    'body': '#{body.gsub(/^k/, 'K')}'
+  EOS
+end
+
+def process_node(node)
+  snippets = []
+  case node.name
+  when 'struct'
+    # do nothing.
+  when 'cftype'
+    # do nothing.
+  when 'constant'
+    snippets << generate_snippet("#{node[:name]} (#{node[:declared_type]})", node[:name], node[:name])
+  when 'enum'
+    snippets << generate_snippet("#{node[:name]} (#{node[:value]})", node[:name], node[:name])
+  when 'function'
+    name = node[:name]
+    trigger = node[:name]
+    args = node.xpath('./arg').map.with_index {|n, i|
+      "${#{i+1}:#{n[:declared_type]} #{n[:name]}}"
+    }
+    body = "#{name}(#{args.join(', ')})"
+    snippets << generate_snippet(name, trigger, body)
+  when 'class', 'informal_protocol'
+    node.xpath('./method').each do |method_node|
+      if method_node[:class_method] == 'true'
+        name = "#{node[:name]}.#{method_node[:selector]}"
+      else
+        name = method_node[:selector]
+      end
+      trigger = method_node[:selector]
+      args = method_node.xpath('./arg').map.with_index {|n, i|
+        "${#{i+1}:#{n[:declared_type]} #{n[:name]}}"
+      }
+      body = "#{name}(#{args.join(', ')})"
+      snippets << generate_snippet(name, trigger, body)
+    end
+  when 'depends_on'
+    # do nothing.
+  when 'opaque'
+    # do nothing.
+  when 'string_constant'
+    snippets << generate_snippet("#{node[:name]} (#{node[:value]})", node[:name], node[:name])
+  when 'function_alias'
+    snippets << generate_snippet("#{node[:name]} (#{node[:original]})", node[:name], node[:name])
+  else
+    STDERR.puts "\e[31mUnknown node: #{node.name}\e[0m"
+  end
+  snippets
+end
+
+File.open('../snippets/cocoatouch.cson', 'w') {|f|
+  f.puts "'.source.rubymotion':"
+
+  BRIDGE_SUPPORT_FILES[:ios].each do |file|
+    doc = Nokogiri::XML(File.read(file))
+    doc.xpath('/signatures/*').each do |node|
+      f.puts process_node(node)
+    end
+  end
+}
+
+File.open('../snippets/cocoa.cson', 'w') {|f|
+  f.puts "'.source.rubymotion':"
+
+  BRIDGE_SUPPORT_FILES[:osx].each do |file|
+    doc = Nokogiri::XML(File.read(file))
+    doc.xpath('/signatures/*').each do |node|
+      f.puts process_node(node)
+    end
+  end
+}
